5f003fbf48003c74af6f6d610e09a31b0f2896bb
MundiaNderi <sarahngima77@gmail.com>
2023-03-14 10:38:21 +0300

    task 1

--- pycode01.py
+++ pycode01.py
@@ -9,9 +9,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(res))
     return res
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
